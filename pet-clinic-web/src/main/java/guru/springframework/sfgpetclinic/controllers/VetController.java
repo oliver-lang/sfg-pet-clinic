eaab0cd948c227da9d73c25a983ffda578de5276
oliver <dr.oliver.lang@gmail.com>
2020-08-16 12:15:07 +0200

    Fixing Broken Links. #45

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -17,11 +17,9 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model){
-
         model.addAttribute("vets", vetService.findAll());
-
         return "vets/index";
     }
 }
